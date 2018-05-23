b51a7d252442372e5213521311e267efec25576c
kjamartens <koen.martens@wur.nl>
2018-05-23 13:00:57 +0200

    miCube.html added for access to miCube-only page
    Signed-off-by: kjamartens <koen.martens@wur.nl>

--- miCube/javascript/update_figures.js
+++ miCube/javascript/update_figures.js
@@ -2,7 +2,7 @@
 // Figure Exploded interactively loads static images, stored locally
 function update_figure_exploded() {
   var period = document.getElementById("Figure_exploded_period_slider").value;
-  var filename = "./images/miCube_exploded_animation/smallSize/exploded_" + period + ".png";
+  var filename = "./miCube/images/miCube_exploded_animation/smallSize/exploded_" + period + ".png";
   var image = document.getElementById("Figure_exploded_image");
   image.src = filename;
 }
@@ -17,7 +17,7 @@ function update_figure_rotated() {
   }else{
 	  id = 0;
   }
-  var filename = "./images/miCube_rotated_animation/smallSize/rotated_" + id + ".png";
+  var filename = "./miCube/images/miCube_rotated_animation/smallSize/rotated_" + id + ".png";
   var image = document.getElementById("Figure_rotated_image");
   image.src = filename;
 }
