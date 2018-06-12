af533903c84daf730d61cbce1cb3e54db85fbcf7
kjamartens <koen.martens@wur.nl>
2018-06-12 17:21:47 +0200

    Mostly changes to Component table - Added LED lights - Added filter block - Moved columns around
    Changes to header
    
    Added page related projects - page has to be written yet
    
    Signed-off-by: kjamartens <koen.martens@wur.nl>

--- miCube/header.js
+++ miCube/header.js
@@ -2,12 +2,18 @@ document.write('\
 <h1 class="header">#miCube: Rethinking modular fluorescence microscopy</h1>\
         <ul>\
 		<li class="first"><a href="./index.html" target="_self">Main page</a></li>\
-		<li class="first"><a href="./micube_description.html" target="_self">miCube description</a></li>\
+		<!--<li class="first"><a href="./micube_description.html" target="_self">miCube description</a></li>-->\
 		<li class="first"><a href="./component_table.html" target="_self">Component overview</a></li>\
-		<br><hr style=" margin: 0.1em auto;">\
-			<font style="line-height:0.5em;">\
-          <a class="buttons github" href="https://github.com/HohlbeinLab/miCube" style="padding-top:0px;">View On GitHub</a>&nbsp;&nbsp;&nbsp;\
-		  <a class="buttons pdf" href="" style="margin-bottom:0px; align:right;">\
-Download PDF</a></font>\
+		<li class="first"><a href="./related_projects.html" target="_self">Related projects</a></li>\
+		<div style="padding-top:10px;"></div><hr style=" margin: 0.1em auto;"><div style="padding-top:10px;"></div>\
+			<font style="line-height:1.5em;">\
+          <table>\
+		  <tr><td>\
+		  <a class="buttons github" href="https://github.com/HohlbeinLab/miCube" style="padding-top:0px;">View On GitHub</a></td><td>&nbsp;&nbsp;&nbsp;</td><td>\
+		  <a class="buttons pdf" href="" style="margin-bottom:0px; align:right;">Download PDF</a></td></tr>\
+<tr><td><a class="buttons twitter" href="https://twitter.com/HohlbeinLab" style="padding-top:0px;">Twitter</a></td><td>&nbsp;&nbsp;&nbsp;</td><td>\
+		  <a class="buttons lab" href="http://www.jhohlbein.com" style="margin-bottom:0px; align:right;">\
+Hohlbein lab</a></td></tr></table>\
+</font>\
         </ul>\
 ');
