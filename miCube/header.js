ea790e4458c27eede28038a41f2a9a1926f1c0bf
kjamartens <koen.martens@wur.nl>
2018-06-13 16:46:49 +0200

    Added readme to /miCube folder Added numbers in text on component table Removed PDF from header
    Signed-off-by: kjamartens <koen.martens@wur.nl>

--- miCube/header.js
+++ miCube/header.js
@@ -10,10 +10,10 @@ document.write('\
 		<font style="line-height:1.5em;">\
         <table>\
 		<tr><td>\
-		<a class="buttons github" href="https://github.com/HohlbeinLab/miCube" style="padding-top:0px;">View On GitHub</a></td><td>&nbsp;&nbsp;&nbsp;</td><td>\
-		<a class="buttons pdf" href="" style="margin-bottom:0px; align:right;">Download PDF</a></td></tr>\
-		<tr><td><a class="buttons twitter" href="https://twitter.com/HohlbeinLab" style="padding-top:0px;">Twitter</a></td><td>&nbsp;&nbsp;&nbsp;</td><td>\
-		<a class="buttons lab" href="http://www.jhohlbein.com" style="margin-bottom:0px; align:right;">Hohlbein lab</a></td></tr>\
+		<a class="buttons github" href="https://github.com/HohlbeinLab/HohlbeinLab.github.io/tree/master/miCube" style="padding-top:0px;">View On GitHub</a></td><td>&nbsp;&nbsp;&nbsp;</td><td>\
+		<a class="buttons twitter" href="https://twitter.com/HohlbeinLab" style="padding-top:0px;">Twitter</a></td></tr>\
+		<tr><td><a class="buttons lab" href="http://www.jhohlbein.com" style="margin-bottom:0px; align:right;">Hohlbein lab</a></td><td>&nbsp;&nbsp;&nbsp;</td><td>\
+		<!--<a class="buttons pdf" href="" style="margin-bottom:0px; align:right;">Download PDF</a>--></td></tr>\
 		<tr><td colspan="3"><a class="buttons bip" href="https://www.wur.nl/en/Research-Results/Chair-groups/Agrotechnology-and-Food-Sciences/Laboratory-of-Biophysics.htm" style="padding-top:0px;">Wageningen University: Biophysics</a></td></tr>\
 </table>\
 </font>\
