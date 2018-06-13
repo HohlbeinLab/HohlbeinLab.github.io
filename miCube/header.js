059bc560c3478f1f488c632a44c577a7866bfb34
kjamartens <koen.martens@wur.nl>
2018-06-13 14:22:54 +0200

    Added phasor page Updating related projects Checking firefox font issues
    Signed-off-by: kjamartens <koen.martens@wur.nl>

--- miCube/header.js
+++ miCube/header.js
@@ -4,7 +4,8 @@ document.write('\
 		<li class="first"><a href="./index.html" target="_self">Main page</a></li>\
 		<!--<li class="first"><a href="./micube_description.html" target="_self">miCube description</a></li>-->\
 		<li class="first"><a href="./component_table.html" target="_self">Component overview</a></li>\
-		<li class="first"><a href="./related_projects.html" target="_self">Related projects</a></li>\
+		<li class="first"><a href="./phasor.html" target="_self">Phasor-based single molecule localization</a></li>\
+		<li class="first"><a href="./related_projects.html" target="_self">Related projects and instruments</a></li>\
 		<div style="padding-top:10px;"></div><hr style=" margin: 0.1em auto;"><div style="padding-top:10px;"></div>\
 			<font style="line-height:1.5em;">\
           <table>\
