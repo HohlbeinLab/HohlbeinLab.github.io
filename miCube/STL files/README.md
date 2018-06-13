ba0db9514715232898718a9fe6e49a6cb7b21a58
kjamartens <koen.martens@wur.nl>
2018-06-13 15:00:45 +0200

    Added STL files here Added link to BIP wur Added link to York template Slightly altered footer
    Signed-off-by: kjamartens <koen.martens@wur.nl>

---
+++ miCube/STL files/README.md
@@ -0,0 +1,3 @@
+# STL files of all custom miCube components
+
+The id of each component is corresponding to the numbers used in the <a href="https://hohlbeinlab.github.io/miCube/component_table.html">miCube component table</a>.
--- miCube/header.js
+++ miCube/header.js
@@ -7,14 +7,15 @@ document.write('\
 		<li class="first"><a href="./phasor.html" target="_self">Phasor-based single molecule localization</a></li>\
 		<li class="first"><a href="./related_projects.html" target="_self">Related projects and instruments</a></li>\
 		<div style="padding-top:10px;"></div><hr style=" margin: 0.1em auto;"><div style="padding-top:10px;"></div>\
-			<font style="line-height:1.5em;">\
-          <table>\
-		  <tr><td>\
-		  <a class="buttons github" href="https://github.com/HohlbeinLab/miCube" style="padding-top:0px;">View On GitHub</a></td><td>&nbsp;&nbsp;&nbsp;</td><td>\
-		  <a class="buttons pdf" href="" style="margin-bottom:0px; align:right;">Download PDF</a></td></tr>\
-<tr><td><a class="buttons twitter" href="https://twitter.com/HohlbeinLab" style="padding-top:0px;">Twitter</a></td><td>&nbsp;&nbsp;&nbsp;</td><td>\
-		  <a class="buttons lab" href="http://www.jhohlbein.com" style="margin-bottom:0px; align:right;">\
-Hohlbein lab</a></td></tr></table>\
+		<font style="line-height:1.5em;">\
+        <table>\
+		<tr><td>\
+		<a class="buttons github" href="https://github.com/HohlbeinLab/miCube" style="padding-top:0px;">View On GitHub</a></td><td>&nbsp;&nbsp;&nbsp;</td><td>\
+		<a class="buttons pdf" href="" style="margin-bottom:0px; align:right;">Download PDF</a></td></tr>\
+		<tr><td><a class="buttons twitter" href="https://twitter.com/HohlbeinLab" style="padding-top:0px;">Twitter</a></td><td>&nbsp;&nbsp;&nbsp;</td><td>\
+		<a class="buttons lab" href="http://www.jhohlbein.com" style="margin-bottom:0px; align:right;">Hohlbein lab</a></td></tr>\
+		<tr><td colspan="3"><a class="buttons bip" href="https://www.wur.nl/en/Research-Results/Chair-groups/Agrotechnology-and-Food-Sciences/Laboratory-of-Biophysics.htm" style="padding-top:0px;">Wageningen University: Biophysics</a></td></tr>\
+</table>\
 </font>\
         </ul>\
 ');
