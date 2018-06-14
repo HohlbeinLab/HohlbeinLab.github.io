c281f53423c9e4b8c92f1b0ac7369ca53eaa2b17
kjamartens <koen.martens@wur.nl>
2018-06-14 11:09:15 +0200

    Added people of miCube Changed header sizing a little
    Signed-off-by: kjamartens <koen.martens@wur.nl>

--- miCube/header.js
+++ miCube/header.js
@@ -1,11 +1,12 @@
 document.write('\
-<h1 class="header">#miCube: Rethinking modular fluorescence microscopy</h1>\
+<h1 class="header">#miCube: Modular fluorescence microscopy</h1>\
         <ul>\
 		<li class="first"><a href="./index.html" target="_self">Main page</a></li>\
 		<!--<li class="first"><a href="./micube_description.html" target="_self">miCube description</a></li>-->\
 		<li class="first"><a href="./component_table.html" target="_self">Component overview</a></li>\
 		<li class="first"><a href="./phasor.html" target="_self">Phasor-based single molecule localization</a></li>\
 		<li class="first"><a href="./related_projects.html" target="_self">Related projects and instruments</a></li>\
+		<li class="first"><a href="./people.html" target="_self">People of miCube</a></li>\
 		<div style="padding-top:10px;"></div><hr style=" margin: 0.1em auto;"><div style="padding-top:10px;"></div>\
 		<font style="line-height:1.5em;">\
         <table>\
